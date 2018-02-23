ccb4f36e3fbd4e35a3f9924c75a23b89715290ac
mvanack <mv2640@columbia.edu>
2018-02-23 10:06:58 -0500

    updated setup.py to be personal

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="helloworld",
     version="0.1",
     packages=find_packages(),
+    author="Meredith VanAcker",
+    license="GPLv3",
+    description="A package for prepping for a movie",
     entry_points={
         'console_scripts': ['helloworld = helloworld.__main__:main']
         }
